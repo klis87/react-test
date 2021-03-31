6fe8a634a9ae7f7cccb5483237443559b449af76
klis87 <klisiczynski@gmail.com>
2021-04-01 00:25:43 +0200

    Modified test content

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-test
+
+1) Install dependencies
+2) Launch application with `yarn run start` or `npm run start`.
+3) Follow the next instructions in the app itself. 
---
+++ src/todos.js
@@ -0,0 +1,6 @@
+export const todos = [
+  { title: 'TODO 1', description: 'Description 1', completed: false },
+  { title: 'TODO 2', description: 'Description 2', completed: false },
+  { title: 'TODO 3', description: 'Description 3', completed: false },
+  { title: 'TODO 4', description: 'Description 4', completed: false },
+];
